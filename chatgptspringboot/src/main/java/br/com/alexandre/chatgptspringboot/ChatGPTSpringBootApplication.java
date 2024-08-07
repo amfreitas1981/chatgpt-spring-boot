80d02f0778fa84f5022fe594ae443f215e73d785
Alexandre Freitas <alexandremfreitas@gmail.com>
2024-08-07 00:06:47 -0300

    Initial version

---
+++ chatgptspringboot/src/main/java/br/com/alexandre/chatgptspringboot/ChatGPTSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.alexandre.chatgptspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatGPTSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatGPTSpringBootApplication.class, args);
+	}
+
+}
---
+++ chatgptspringboot/src/test/java/br/com/alexandre/chatgptspringboot/ChatGPTSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alexandre.chatgptspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatGPTSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
